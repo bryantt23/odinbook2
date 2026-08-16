11e09ef4f95746240f57a80c4b3e4c6e47f99705
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-08-16 17:40:42 +0800

    add signed-in and guest home routes and nav

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  devise_scope :user do
+    authenticated :user do
+      root to: "users#index", as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
   # Defines the root path route ("/")
-  root "users#index"
 end
