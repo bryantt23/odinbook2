d8a65f6136dfede3213eae70a92da61135785d41
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-09-09 00:06:32 +0800

    add content to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_08_16_094429) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_08_160544) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "content"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "subject"
     t.text "content"
@@ -32,5 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_08_16_094429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
