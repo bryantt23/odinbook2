771ec88eca7d7106c359ea6172cc8b9d8fdaabe2
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-09-10 06:23:56 +0800

    add comment creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :comments, only: [ :create ]
+  end
   resources :users, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
