58149e264ef957daa60350cd52ce8959bb1fc743
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-09-16 04:43:49 +0800

    add post liking

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
     resources :comments, only: [ :create ]
+    resources :likes, only: [ :create ]
   end
   resources :users, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
