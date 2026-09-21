d8e84bd4036c45b658a9ef4d69da326924812d9e
bryant tunbutr <bryantt23@users.noreply.github.com>
2026-09-22 02:22:28 +0800

    prevent users from following themselves

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  validate :cant_friend_self
+
+  private
+  def cant_friend_self
+    if user_id == friend_id
+      errors.add(:friend_id, "cannot friend yourself")
+    end
+  end
 end
